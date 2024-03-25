14248f58f8d57c7755c6fe3a7d9c73b5250cadd1
nshksghh09 <100795838+nshksghh09@users.noreply.github.com>
2024-03-25 16:50:07 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://anushkasingh09:kGtxW623RtVAD9KP@cluster0.qjuweye.mongodb.net/todo-app-data"
+  ""
 );
 
 const todoSchema = mongoose.Schema({
